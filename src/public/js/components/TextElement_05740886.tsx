22a9f475ca0c5a41c979a1c97e59ae5a73dc9fab
Jatupon Sukkasem <jasukkas.th@gmail.com>
2020-06-21 19:37:20 +0700

    Use dot notation feature.

--- src/public/js/components/TextElement_05740886.tsx
+++ src/public/js/components/TextElement_05740886.tsx
@@ -54,6 +54,14 @@ class TextElement_05740886 extends Base {
     super(props);
     this.state = CodeHelper.clone(DefaultState);
     
+    // Load and assign to this.state.data:
+    // 
+    this.load("User.email");
+    
+    // Make changes to this.state.data and save using:
+    // 
+    // this.save("User.email");
+    
     this.initialize();
   }
   // <---Auto[ClassBegin]
@@ -75,8 +83,8 @@ class TextElement_05740886 extends Base {
   // Auto[ClassEnd]--->
   protected render(): any {
     return (
-      <div className={"internal-fsb-element col-3 offset-7 " + (this.props.forward && this.props.forward.classes || '')} internal-fsb-guid="05740886" style={Object.assign({'marginTop': '25px'}, this.props.forward && this.props.forward.styles || {})}>
-        {this.getDataFromNotation('User.email')}
+      <div className={"internal-fsb-element col-3 offset-7 " + (this.props.forward && this.props.forward.classes || '')} internal-fsb-guid="05740886" style={Object.assign({'marginTop': '25px'}, this.props.forward && this.props.forward.styles || {})} [object Object]>
+        Email Address
       </div>
     )
   }
