ff8e4fda021da4d9058e3292137120c6a9281a38
Jatupon Sukkasem <jasukkas.th@gmail.com>
2020-06-21 11:14:47 +0700

    Call session.save() when logging in, too.

--- src/public/js/Site.tsx
+++ src/public/js/Site.tsx
@@ -5,7 +5,6 @@ import {Project, DeclarationHelper} from './helpers/DeclarationHelper.js';
 import {DataManipulationHelper} from './helpers/DataManipulationHelper.js';
 import {HTMLHelper} from './helpers/HTMLHelper.js';
 import {EventHelper} from './helpers/EventHelper.js';
-import './components/TextElement_05740886.js';
 import './components/LoginControl.js';
 
 declare let React: any;
