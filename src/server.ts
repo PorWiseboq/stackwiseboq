cf57498731b363438d082f869b62d636525730e2
Jatupon Sukkasem <jatupon@softenstorm.com>
2020-08-25 09:08:25 +0700

    Comment out unused code.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import errorHandler from "errorhandler";
 
 import {app} from "./app";
-import {NotificationHelper} from "./controllers/helpers/NotificationHelper.js";
+//import {NotificationHelper} from "./controllers/helpers/NotificationHelper.js";
 
 /**
  * Error Handler. Provides full stack - remove for production
@@ -11,13 +11,13 @@ app.use(errorHandler());
 /**
  * Start Express server.
  */
-const server = app.listen(app.get("port"), () => {
+/*const server = app.listen(app.get("port"), () => {
   console.log(
     "  App is running at http://localhost:%d in %s mode",
     app.get("port"),
     app.get("env")
   );
   console.log("  Press CTRL-C to stop\n");
-}); 
+});*/ 
 
 export default server;
