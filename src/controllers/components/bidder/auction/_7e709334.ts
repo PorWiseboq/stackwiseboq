fda4dd21197aa35ae515773b0c53f07bc56c0fe7
Jatupon Sukkasem <jatupon@softenstorm.com>
2020-09-05 14:49:41 +0700

    [continue]

--- src/controllers/components/bidder/auction/_7e709334.ts
+++ src/controllers/components/bidder/auction/_7e709334.ts
@@ -234,7 +234,7 @@ WHERE DATE_ADD(createdAt, interval IF(hours = NULL, 24, hours) hour) < now() AND
      		      'Auction.Store.sid': null,
      		      'Auction.Quote.qid': null,
      		      'Auction.Quote.User.id': null
-     		    }), ProjectConfigurationHelper.getDataSchema().tables['User'], {});
+     		    }), ProjectConfigurationHelper.getDataSchema().tables['Auction'], {});
      		    let userDataset = auctionDataset['Auction'].rows[0].relations['Quote'].rows[0].relations;
      		    let storeDataset = auctionDataset['Auction'].rows[0].relations;
      		    
