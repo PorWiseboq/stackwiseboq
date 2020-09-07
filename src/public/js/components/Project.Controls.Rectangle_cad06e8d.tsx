cff6fd981116b0452ae3550503c385bb3b76a1be
Jatupon Sukkasem <jatupon@softenstorm.com>
2020-09-07 16:56:57 +0700

    Add forward-transfering info.

--- src/public/js/components/Project.Controls.Rectangle_cad06e8d.tsx
+++ src/public/js/components/Project.Controls.Rectangle_cad06e8d.tsx
@@ -1040,23 +1040,48 @@ class Rectangle_cad06e8d extends Base {
                                           | #{this.getDataFromNotation('Quote[#i].Auction.promotion') || 'ไม่มี'}
                                   .internal-fsb-element.-fsb-preset-1715aae1.col-12.offset-0(style={'FsbInheritedPresets': '1715aae1'}, internal-fsb-guid="5428078e")
                                     | คุณได้เสนอราคาไว้ที่ #{(this.getDataFromNotation('Quote[#i].Auction.price') || 0).toFixed(2)} บาท
-                                  .internal-fsb-element.offset-3.col-6(style={'marginTop': '15px', 'paddingLeft': '0px', 'paddingRight': '0px', 'fontSize': '13px', display: (()=>{return this.getQuoteTypeDisplay(QuoteType.PAID, true);})()}, internal-fsb-guid="4a9c1bd6")
+                                  .internal-fsb-element.col-12(style={'paddingLeft': '0px', 'paddingRight': '0px', display: (()=>{return this.getQuoteTypeDisplay(QuoteType.PAID, true);})()}, internal-fsb-guid="782a46eb")
                                     .container-fluid
                                       .row.internal-fsb-strict-layout.internal-fsb-allow-cursor
-                                        .internal-fsb-element.col-5.offset-0(style={'textAlign': 'right', 'fontWeight': 'bold'}, internal-fsb-guid="97c4a491")
-                                          | จากธนาคาร:
-                                        .internal-fsb-element.col-7.offset-0(internal-fsb-guid="c3413167")
-                                          | #{this.getDataFromNotation('Quote[#i].Auction.Payment.Transfer.origin')}
-                                        .internal-fsb-element.col-5.offset-0(style={'textAlign': 'right', 'fontWeight': 'bold'}, internal-fsb-guid="088400aa")
-                                          | ชื่อผู้โอน:
-                                        .internal-fsb-element.col-7.offset-0(internal-fsb-guid="2ac784b2")
-                                          | #{this.getDataFromNotation('Quote[#i].Auction.Payment.Transfer.transferrer')}
-                                        .internal-fsb-element.col-5.offset-0(style={'textAlign': 'right', 'fontWeight': 'bold'}, internal-fsb-guid="028ad007")
-                                          | เวลา:
-                                        .internal-fsb-element.col-7.offset-0(internal-fsb-guid="1566119e")
-                                          | #{this.getDataFromNotation('Quote[#i].Auction.Payment.Transfer.time')}
-                                  .internal-fsb-element.-fsb-preset-1715aae1.col-12(style={'FsbInheritedPresets': '1715aae1', display: (()=>{return this.getQuoteTypeDisplay(QuoteType.PAID, true);})()}, internal-fsb-guid="36ed3d83")
-                                    | ทาง WiseBOQ กำลังเช็คยอดเงินโอนและกำลังโอนเงินให้ทางร้านค้า
+                                        .internal-fsb-element.offset-3.col-6(style={'marginTop': '15px', 'paddingLeft': '0px', 'paddingRight': '0px', 'fontSize': '13px'}, internal-fsb-guid="4a9c1bd6")
+                                          .container-fluid
+                                            .row.internal-fsb-strict-layout.internal-fsb-allow-cursor
+                                              .internal-fsb-element.col-5.offset-0(style={'textAlign': 'right', 'fontWeight': 'bold'}, internal-fsb-guid="97c4a491")
+                                                | จากธนาคาร:
+                                              .internal-fsb-element.col-7.offset-0(internal-fsb-guid="c3413167")
+                                                | #{this.getDataFromNotation('Quote[#i].Auction.Payment.Transfer.origin')}
+                                              .internal-fsb-element.col-5.offset-0(style={'textAlign': 'right', 'fontWeight': 'bold'}, internal-fsb-guid="088400aa")
+                                                | ชื่อผู้โอน:
+                                              .internal-fsb-element.col-7.offset-0(internal-fsb-guid="2ac784b2")
+                                                | #{this.getDataFromNotation('Quote[#i].Auction.Payment.Transfer.transferrer')}
+                                              .internal-fsb-element.col-5.offset-0(style={'textAlign': 'right', 'fontWeight': 'bold'}, internal-fsb-guid="028ad007")
+                                                | เวลา:
+                                              .internal-fsb-element.col-7.offset-0(internal-fsb-guid="1566119e")
+                                                | #{this.getDataFromNotation('Quote[#i].Auction.Payment.Transfer.time')}
+                                        .internal-fsb-element.-fsb-preset-1715aae1.col-12(style={'FsbInheritedPresets': '1715aae1', display: (()=>{return (this.getDataFromNotation('Quote[#i].Auction.Payment.Transfer.status') == 0) ? 'block' : 'none';})()}, internal-fsb-guid="36ed3d83")
+                                          | ทาง WiseBOQ กำลังเช็คยอดเงินโอนและกำลังโอนเงินให้ทางร้านค้า
+                                        .internal-fsb-element.col-12.-fsb-preset-1715aae1(style={'FsbInheritedPresets': '1715aae1', 'display': (()=>{return (this.getDataFromNotation('Quote[#i].Auction.Payment.Transfer.status') == 1) ? 'block' : 'none';})() || 'none'}, internal-fsb-guid="07623084")
+                                          | ทาง WiseBOQ ตรวจเช็คยอดเงินโอนเสร็จแล้วและกำลังโอนเงินให้ทางร้านค้า
+                                        .internal-fsb-element.col-12.-fsb-preset-1715aae1(style={'FsbInheritedPresets': '1715aae1', 'display': (()=>{return (this.getDataFromNotation('Quote[#i].Auction.Payment.Transfer.status') == 2) ? 'block' : 'none';})() || 'none'}, internal-fsb-guid="323e3062")
+                                          | ทาง WiseBOQ โอนเงินให้ทางร้านค้าเรียบร้อยแล้ว
+                                        .internal-fsb-element.col-12(style={'paddingLeft': '0px', 'paddingRight': '0px', display: (()=>{return (this.getDataFromNotation('Quote[#i].Auction.Payment.Transfer.status') == 2) ? 'block' : 'none';})()}, internal-fsb-guid="70267cd8")
+                                          .container-fluid
+                                            .row.internal-fsb-strict-layout.internal-fsb-allow-cursor
+                                              .internal-fsb-element.col-6.offset-3(style={'marginTop': '15px', 'paddingLeft': '0px', 'paddingRight': '0px', 'fontSize': '13px'}, internal-fsb-guid="9c819cb9")
+                                                .container-fluid
+                                                  .row.internal-fsb-strict-layout.internal-fsb-allow-cursor
+                                                    .internal-fsb-element.col-5(style={'fontWeight': 'bold', 'textAlign': 'right'}, internal-fsb-guid="db073cac")
+                                                      | จากธนาคาร:
+                                                    .internal-fsb-element.col-7(internal-fsb-guid="de0a7775")
+                                                      | #{this.getDataFromNotation('Quote[#i].Auction.Payment.Transfer.forwardingOrigin')}
+                                                    .internal-fsb-element.col-5(style={'fontWeight': 'bold', 'textAlign': 'right'}, internal-fsb-guid="a008a287")
+                                                      | ชื่อผู้โอน:
+                                                    .internal-fsb-element.col-7(internal-fsb-guid="85c1b9e0")
+                                                      | #{this.getDataFromNotation('Quote[#i].Auction.Payment.Transfer.forwardingTransferrer')}
+                                                    .internal-fsb-element.col-5(style={'fontWeight': 'bold', 'textAlign': 'right'}, internal-fsb-guid="5ac563a1")
+                                                      | เวลา:
+                                                    .internal-fsb-element.col-7(internal-fsb-guid="15d9bc67")
+                                                      | #{this.getDataFromNotation('Quote[#i].Auction.Payment.Transfer.forwardingTime')}
                                   Button.internal-fsb-element.internal-fsb-allow-cursor.col-4.offset-4.btn.btn-danger.btn-sm(style={'marginTop': '10px', 'marginBottom': '10px', display: (()=>{return (this.getDataFromNotation('Quote[#i].Auction.cancelled')) ? 'none' : 'block';})()}, onClick=((event) => { window.internalFsbSubmit('d3e31c36', 'Auction', event, ((results) => { this.manipulate('d3e31c36', 'Auction', results); }).bind(this)); }).bind(this), type="button", internal-fsb-guid="d3e31c36")
                                     .internal-fsb-element(internal-fsb-guid="d3e31c36-text")
                                       | ยกเลิก
