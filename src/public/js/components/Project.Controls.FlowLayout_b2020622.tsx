0bb2330f065650354f4c442e9b7d2b5cb5de2fc7
Jatupon Sukkasem <jatupon@softenstorm.com>
2020-09-18 15:52:24 +0700

    [continue]

--- src/public/js/components/Project.Controls.FlowLayout_b2020622.tsx
+++ src/public/js/components/Project.Controls.FlowLayout_b2020622.tsx
@@ -639,7 +639,7 @@ class FlowLayout_b2020622 extends Base {
                           .container-fluid
                             .row.internal-fsb-strict-layout.internal-fsb-allow-cursor
                               .internal-fsb-element.col-2.offset-0.-fsb-preset-b6c9ad89(style={padding: '0px'}, internal-fsb-guid="12403b79")
-                                input(style={'display': 'block', 'FsbInheritedPresets': 'b6c9ad89'}, ref="pickup1", onClick=this.onRadioClick_12403b79.bind(this), type="radio", name="delivery", value="true", required=true, disabled=this.state.disabled, defaultChecked=this.getDataFromNotation("Quote.pickup") == 'true')
+                                input(style={'display': 'block', 'FsbInheritedPresets': 'b6c9ad89'}, ref="pickup1", onClick=this.onRadioClick_12403b79.bind(this), type="radio", name="delivery", value="true", required=true, disabled=this.state.disabled)
                               .internal-fsb-element.col-10.offset-0.-fsb-preset-b5cd72c0(style={'FsbInheritedPresets': 'b5cd72c0'}, internal-fsb-guid="b8a9d038")
                                 | ไปรับสินค้าด้วยตนเอง
                               .internal-fsb-element.col-10.offset-2.-fsb-preset-e51ca172(style={'FsbInheritedPresets': 'e51ca172'}, internal-fsb-guid="c00c3c67")
@@ -651,7 +651,7 @@ class FlowLayout_b2020622 extends Base {
                           .container-fluid
                             .row.internal-fsb-strict-layout.internal-fsb-allow-cursor
                               .internal-fsb-element.col-2.offset-0.-fsb-preset-b6c9ad89(style={padding: '0px'}, internal-fsb-guid="0606ea02")
-                                input(style={'display': 'block', 'FsbInheritedPresets': 'b6c9ad89'}, ref="pickup2", onClick=this.onRadioClick_0606ea02.bind(this), type="radio", name="delivery", value="false", required=true, disabled=this.state.disabled, defaultChecked=this.getDataFromNotation("Quote.pickup") == 'false')
+                                input(style={'display': 'block', 'FsbInheritedPresets': 'b6c9ad89'}, ref="pickup2", onClick=this.onRadioClick_0606ea02.bind(this), type="radio", name="delivery", value="false", required=true, disabled=this.state.disabled)
                               .internal-fsb-element.col-10.offset-0.-fsb-preset-b5cd72c0(style={'FsbInheritedPresets': 'b5cd72c0'}, internal-fsb-guid="94ec51a9")
                                 | จัดส่งสินค้ามาให้
                               .internal-fsb-element.col-10.offset-2.-fsb-preset-e51ca172(style={'FsbInheritedPresets': 'e51ca172'}, internal-fsb-guid="9e55d0d6")
